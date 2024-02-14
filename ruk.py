adb96e2cdac060522e79d1cc537149c439f7da4b
Yonew0 <131365746+Yonew0@users.noreply.github.com>
2024-02-14 18:48:20 +0700

    Add files via upload

---
+++ ruk.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+query = 'cats'
+url = f'https://www.kiddle.co/s.php?q={query}'
+
+page = requests.get(url).text
+soup = BeautifulSoup(page, 'html.parser')
+
+#print(soup, dir(soup))
+
+for raw_img in soup.find_all('img'):
+    link = raw_img('src')
+    if link and link.startswith('https'):
+        response = requests.get(link)
+        with open("./today_avatar.jpg", "wb") as f:
+            f.write(response.content)
